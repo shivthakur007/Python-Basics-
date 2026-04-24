f621493f0f7d36cd29cc1c6b0eb02122b8ec9ec7
Shiv Thakur <shivthakur@Shivs-MacBook-Air.local>
2026-04-24 21:36:41 +0530

    Added string to Int Converter

--- tryandexcept.py
+++ tryandexcept.py
@@ -1,6 +1,16 @@
+# This program demonstrates the use of try and except blocks to handle exceptions.
 try:
     n = int(input("Enter a number: "))
     reult = 10 / n
     print("Result is: ", reult)
 except ZeroDivisionError:
     print("You cannot divide by zero.")
+except ValueError:
+    print("Please enter a valid number.")
+
+#string to Int converter
+try:
+    s = int(input("Enter number: "))
+    print("Square: ", s * s)
+except ValueError:
+    print("Invalid Input") 
